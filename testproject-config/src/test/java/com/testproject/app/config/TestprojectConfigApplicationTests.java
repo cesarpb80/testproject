d32b905042f85a0f2c9bb3f600f8f6a8315133db
DESKTOP-0C2EDO4\César <cesarpb80@gmail.com>
2022-02-02 01:16:40 -0500

    se agrega config

---
+++ testproject-config/src/test/java/com/testproject/app/config/TestprojectConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testproject.app.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestprojectConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
