ce9b341d3e491a869748e7c5268ea97341a19597
DESKTOP-0C2EDO4\César <cesarpb80@gmail.com>
2022-02-11 11:34:45 -0500

    proyecto discovery

---
+++ testproject-discovery/dockerBuild.sh
@@ -0,0 +1,6 @@
+#Shell Script automates the spring boot app docker build
+#!/bin/bash
+docker-compose down
+mvn clean install
+docker build --tag=testproject-discovery -f DockerfileDEV .
+docker images -qf dangling=true | xargs -r docker rmi
---
+++ testproject-discovery/src/test/java/com/testproject/app/discovery/TestprojectDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testproject.app.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestprojectDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
